a20b9bf0144bf15e2a6a39db37a67a0af8489525
Drnutsu <dentistnut@gmail.com>
2014-10-22 17:26:29 +0700

    rebase develop for login system

---
+++ app/views/productView.blade.php
@@ -0,0 +1,20 @@
+<!-- /var/www/html/onlineShoping/app/views/productView.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ProductTest</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" >
+
+<!-- Optional theme -->
+<link rel="stylesheet" href= "{{asset('css/bootstrap-theme.min.css')}}" >
+
+<!-- Latest compiled and minified JavaScript -->
+<script src= "{{asset('js/bootstrap.min.js')}}" ></script>
+</head>
+<body>
+	<div class = "well">
+		test
+	</div>
+</body>
+</html>
