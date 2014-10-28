88ff8e692db83e4b675628a377c37e67b3136c36
n1ghthawk <n.thanaphon.soo@gmail.com>
2014-10-28 13:39:23 +0700

    routes.php cleanup, merge duplicate view template

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends BaseController {
 
 	public function index()
 	{
-		return View::make('shopHome');
+		return View::make('shopHome', array('user' => core\Customer::newFromEloquent(Auth::user())));
 	}
 
 }
