6bc784ca5aef3030315ea6d93ed41f5134fde6a0
Drnutsu <dentistnut@gmail.com>
2014-10-28 15:46:04 +0700

    fixed product test case

--- app/tests/ProductRepoTest.php
+++ app/tests/ProductRepoTest.php
@@ -30,6 +30,8 @@
 			$this->product->shouldReceive('getColor')->once();
 			$this->product->shouldReceive('getSuplier')->once();
 			$this->product->shouldReceive('getAmount')->once();
+			$this->product->shouldReceive('getProductName')->once();
+			$this->product->shouldReceive('getImgPath')->once();
 
 			//Mocking Model Product only call save method.  	
 			$this->mock_product_elo = Mockery::mock('\Product[save]');
